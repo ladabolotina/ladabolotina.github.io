b75c836d2e9e0e9d9db6641a00c1cd07015dea71
ladabolotina <bolotinalada97@gmail.com>
2020-11-12 23:18:49 +0300

    change errors

--- fortis/_html/js/common.js
+++ fortis/_html/js/common.js
@@ -20,11 +20,11 @@ $(function() {
 	});
 
 	//SVG Fallback
-	if(!Modernizr.svg) {
-		$("img[src*='svg']").attr("src", function() {
-			return $(this).attr("src").replace(".svg", ".png");
-		});
-	};
+	// if(!Modernizr.svg) {
+	// 	$("img[src*='svg']").attr("src", function() {
+	// 		return $(this).attr("src").replace(".svg", ".png");
+	// 	});
+	// };
 
 	//E-mail Ajax Send
 	//Documentation & Example: https://github.com/agragregra/uniMail
@@ -52,7 +52,7 @@ $(function() {
 		}
 	} catch(err) {
 
-	};
+	}
 
 	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
 
