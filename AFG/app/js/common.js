ceb6d5337f1537e87dbdd7ebc5b0bf628a648e26
ladabolotina <bolotinalada97@gmail.com>
2020-11-23 17:06:34 +0300

    commit

--- AFG/app/js/common.js
+++ AFG/app/js/common.js
@@ -6,6 +6,22 @@ $(function() {
 		return false;
 	});
 
+	$(".main_footer .toggle_mnu").click(function(){
+		$("html, body").animate({ scrollTop: $(document).height()}, "slow");
+		return false;
+	});
+
+	$(".arrow_bottom").click(function(){
+		$("html, body").animate({ scrollTop: $(".main_head").height()+120 }, "slow");
+		return false;
+	});
+
+	$(".top").click(function(){
+		$("html, body").animate({ scrollTop: 0 }, "slow");
+		return false;
+	});
+
+
 	$(".section_1 .section_content .info_item").equalHeights();
 	$(".section_3 .section_content .info_item").equalHeights();
 	$(".s1_bottom .info_item").equalHeights();
@@ -17,12 +33,60 @@ $(function() {
 			var ths = $(this);
 			setInterval(function() {
 				ths.removeClass("card-off").addClass("card-on");
-			}, 150*index);
+			}, 200*index);
 		});
 	}, {
 		offset : "20%"
 	});
 
+	$(".section_5").waypoint(function() {
+		$(".section_5 .tc_item").each(function(index) {
+			var ths = $(this);
+			setTimeout(function() {
+				var myAnimation = new DrawFillSVG({
+					elementId: "tc-svg" + index
+				});
+				ths.removeClass("").addClass("");
+			}, 500*index);
+		});
+		this.destroy();
+	}, {
+		offset : "20%"
+	});
+
+	$(".slider").owlCarousel({
+		items : 1,
+		nav : true,
+		navText : "",
+		loop : true,
+		smartSpeed : 600,
+		fluidSpeed : 600,
+		autoplaySpeed : 600,
+		navSpeed : 600,
+		dotsSpeed : 600,
+		dragEndSpeed : 600
+	});
+
+	$(".section_head h2, .section_head p").animated("fadeInRight");
+	$(".info_item_wrap").animated("zoomIn");
+	$(".slider .slide").animated("rollIn");
+	$(".homesect.section_8 .forms").animated("fadeInRight");
+
+	$(".section_2").waypoint(function() {
+		$(".s2_item_wrap").each(function(index) {
+			var ths = $(this);
+			setInterval(function() {
+				ths.addClass("on");
+			}, 200*index);
+		});
+	});
+
+	$(".homesect .section_bottom .buttons").click(function() {
+		$("#callback h4").html($(this).text());
+	}).magnificPopup({
+		type: 'inline',
+		mainClass: 'mfp-forms'
+	});
 
 	//SVG Fallback
 	// if(!Modernizr.svg) {
@@ -32,15 +96,14 @@ $(function() {
 	// };
 
 	//E-mail Ajax Send
-	//Documentation & Example: https://github.com/agragregra/uniMail
-	$("form").submit(function() { //Change
+	$(".forms").submit(function() { //Change
 		var th = $(this);
 		$.ajax({
 			type: "POST",
-			url: "mail.php", //Change
+			url: "/mail.php", //Change
 			data: th.serialize()
 		}).done(function() {
-			alert("Thank you!");
+			alert("Спасибо за заявку!");
 			setTimeout(function() {
 				// Done Functions
 				th.trigger("reset");
